a65c71ac47884fbe9d67b3b5ff7a81101eee7f12
Filya <ilya19xx2014@gmail.com>
2021-08-06 17:12:32 +0300

    написал первую модель для стены

--- second_django_project/stena/models.py
+++ second_django_project/stena/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Mes(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
+    published = models.DateTimeField(auto_now_add=True, db_index=True)
+
