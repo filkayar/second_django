03546e02fcd9bc268ee521ea3058ed1312ff23ac
Filya <ilya19xx2014@gmail.com>
2021-08-09 18:10:49 +0300

    добавил модуль для хранения форм приложения "Стена". Создал форму для добавления записей на стену.

---
+++ second_django_project/stena/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import *
+
+
+class MesForm(ModelForm):
+    class Meta:
+        model = Mes
+        fields = ('title', 'content', 'price', 'category')
