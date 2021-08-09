bcb1345cd850837188541c12a83da782e8736285
Filya <ilya19xx2014@gmail.com>
2021-08-09 18:21:02 +0300

    Настроил контроллер для взаимодействия как с заглавной страницей, так и со страницами категорий. Добавил класс-контроллер для формы добавления записей.

--- second_django_project/stena/views.py
+++ second_django_project/stena/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from .models import Mes
+from django.urls import reverse_lazy
+from .models import *
+from django.views.generic.edit import CreateView
+from .forms import *
 
 
-def index(request):
-    context = {'Mes': Mes.objects.all()}
+def index(request, category_id=None):
+    cats = Category.objects.all()
+    cur_cat = Category.objects.get(pk=category_id) if category_id is not None else None
+    if category_id is not None:
+        context = {'Mes': Mes.objects.filter(category=cur_cat)}
+    else:
+        context = {'Mes': Mes.objects.all()}
+    context['cats'] = cats
+    context['cur_cat'] = cur_cat
     return render(request, 'stena/index.html', context)
+
+
+class MesCreateView(CreateView):
+    template_name = 'stena/create.html'
+    form_class = MesForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cats'] = Category.objects.all()
+        return context
 
