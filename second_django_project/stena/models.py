6405d1a253011f87820f39889d7afc7629284de8
Filya <ilya19xx2014@gmail.com>
2021-08-09 18:17:28 +0300

    Указал имена полей модели для админ-панели. Создал вторую модель - "Категории". Связал сообщения с категориями, к которым они относятся.

--- second_django_project/stena/models.py
+++ second_django_project/stena/models.py
@@ -5,8 +5,25 @@ from django.db import models
 
 
 class Mes(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField(null=True, blank=True)
-    price = models.FloatField(null=True, blank=True)
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
+    title = models.CharField(max_length=50, verbose_name='Заголовок')
+    content = models.TextField(null=True, blank=True, verbose_name='Содержание сообщения')
+    price = models.FloatField(null=True, blank=True, verbose_name='Цена (если указана)')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Дата публикации')
+    category = models.ForeignKey('Category', related_name='notes' , null=True, on_delete=models.PROTECT, verbose_name='Категория')
 
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+        ordering = ['-published']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25, db_index=True, verbose_name='Категория')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Категории'
+        verbose_name = 'Категория'
+        ordering = ['name']
