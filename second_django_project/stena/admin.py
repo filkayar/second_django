296e800ea3e01356d5cc3e69a800dfcefac51932
Filya <ilya19xx2014@gmail.com>
2021-08-09 18:13:05 +0300

    Зарегистрировал модели в админ-панели. Настроил их отображение.

--- second_django_project/stena/admin.py
+++ second_django_project/stena/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class MesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_filter = ('title', 'id', 'published')
+
+
+admin.site.register(Mes, MesAdmin)
+admin.site.register(Category)
