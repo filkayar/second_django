91c79670b9da161748412aec80ace0d5731dff6e
Filya <ilya19xx2014@gmail.com>
2021-08-20 12:42:18 +0300

    Убрал лишний фрагмент адреса для перехода на уровень приложения.

--- second_django_project/second_django_project/settings.py
+++ second_django_project/second_django_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'stena.apps.StenaConfig',
+    'stena',
 ]
 
 MIDDLEWARE = [
