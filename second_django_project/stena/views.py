49f2efcd0fbca9562588acbb504b6f627e7c9803
Filya <ilya19xx2014@gmail.com>
2021-08-26 20:20:45 +0300

    Подключил метод для генерации 404 ошибки при переходе на адрес удаленной новости

--- second_django_project/stena/views.py
+++ second_django_project/stena/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from .models import *
 from django.views.generic.edit import CreateView
