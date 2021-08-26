f471f5bf0c9afadbb126ce51792d654694ed6daa
Filya <ilya19xx2014@gmail.com>
2021-08-26 20:24:40 +0300

    После подключения тега в шаблон отпала необходимость подгрузки категорий непосредственно из контроллеров

--- second_django_project/stena/views.py
+++ second_django_project/stena/views.py
@@ -6,13 +6,11 @@ from .forms import *
 
 
 def index(request, category_id=None):
-    cats = Category.objects.all()
     cur_cat = Category.objects.get(pk=category_id) if category_id is not None else None
     if category_id is not None:
         context = {'Mes': Mes.objects.filter(category=cur_cat)}
     else:
         context = {'Mes': Mes.objects.all()}
-    context['cats'] = cats
     context['cur_cat'] = cur_cat
     return render(request, 'stena/index.html', context)
 
@@ -26,9 +24,3 @@ class MesCreateView(CreateView):
     template_name = 'stena/create.html'
     form_class = MesForm
     success_url = reverse_lazy('index')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['cats'] = Category.objects.all()
-        return context
-
