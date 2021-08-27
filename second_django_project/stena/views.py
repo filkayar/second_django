30d46c81fbd35678f75471a44ab6a89de009d512
Filya <ilya19xx2014@gmail.com>
2021-08-27 18:21:57 +0300

    Попробовал использовать форму не связанную с моделью через контроллер функцию

--- second_django_project/stena/views.py
+++ second_django_project/stena/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from .models import *
 from django.views.generic.edit import CreateView
@@ -24,3 +24,17 @@ class MesCreateView(CreateView):
     template_name = 'stena/create.html'
     form_class = MesForm
     success_url = reverse_lazy('index')
+
+
+# def MesCreateView(request):
+#     if request.method == 'POST':
+#         form = MesForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             try:
+#                 Mes.objects.create(**form.cleaned_data)
+#             except:
+#                 form.add_error(None, 'Ошибка добавления новости')
+#             return redirect('index')
+#     else:
+#         form = MesForm()
+#     return render(request, 'stena/create.html', {'form': form, })
